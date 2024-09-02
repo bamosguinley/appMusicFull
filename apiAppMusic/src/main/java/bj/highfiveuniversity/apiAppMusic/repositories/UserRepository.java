452d984cbae6d4d5fdb218fb7f436794af5da076
Bamos Guinley <guinleyb@gmail.com>
2024-09-02 12:59:28 +0200

    update config

--- apiAppMusic/src/main/java/bj/highfiveuniversity/apiAppMusic/repositories/UserRepository.java
+++ apiAppMusic/src/main/java/bj/highfiveuniversity/apiAppMusic/repositories/UserRepository.java
@@ -1,5 +1,10 @@
 package bj.highfiveuniversity.apiAppMusic.repositories;
 
-public interface UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bj.highfiveuniversity.apiAppMusic.models.User;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long>{
 }
